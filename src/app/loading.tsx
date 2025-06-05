bba37c3bae0cb9a0304bd8e0f335b281c4ae87b0
Roneel V <roneelv974@gmail.com>
2025-06-05 18:33:26 +0000

    Added Loading Screens

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-12rem)] p-4 md:p-8">
+      <div className="animate-pulse text-2xl font-headline text-primary">Loading QuestWheel...</div>
+    </div>
+  );
+}
